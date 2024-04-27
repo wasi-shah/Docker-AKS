b91cafd3ca689fca01b6eb0d0f365f1c20377e51
wasi-shah <51749920+wasi-shah@users.noreply.github.com>
2024-04-27 14:45:54 +0100

    Update

---
+++ AKS DevOps/02 Terraform with Azure/02-demo-resource-group/main.tf
@@ -0,0 +1,16 @@
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
+# Add Tags
+  tags = {
+    "environment" = "k8sdev"
+    "demotag"     = "refreshtest"
+    "main-tag"    = "main-value"
+   # "portal-tag"  = "this is my value"
+
+    }
+}
