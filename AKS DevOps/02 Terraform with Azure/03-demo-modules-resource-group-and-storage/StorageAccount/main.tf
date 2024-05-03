7fdda953fe980d1e850527de0b94bb132598699e
wasi-shah <51749920+wasi-shah@users.noreply.github.com>
2024-05-03 09:27:03 +0100

    Updates

---
+++ AKS DevOps/02 Terraform with Azure/03-demo-modules-resource-group-and-storage/StorageAccount/main.tf
@@ -0,0 +1,15 @@
+resource "random_string" "random" {
+    length = 6
+    special = false
+    upper = false
+}
+
+
+resource "azurerm_storage_account" "example" {
+  name                     = "${lower(var.base_name)}${random_string.random.result}"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+}
---
+++ AKS DevOps/02 Terraform with Azure/04-demo-resource-group-with-variables-using-terraform-and-devops/IAC/variables.tf
@@ -0,0 +1,17 @@
+variable "location" {
+  type        = string
+  default     = "uksouth"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "tf-rg"
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "environment" {
+  type        = string
+  default     = "dev"
+  description = "Sets value for tags tag"
+}
