f639cba1874d01d204b2d1809851ab9f5257ffb4
wasi-shah <51749920+wasi-shah@users.noreply.github.com>
2024-05-11 20:03:01 +0100

    Update main.tf

--- AKS DevOps/02 Terraform with Azure/04-demo-resource-group-with-variables-using-terraform-and-devops/IAC/terraform/main.tf
+++ AKS DevOps/02 Terraform with Azure/04-demo-resource-group-with-variables-using-terraform-and-devops/IAC/terraform/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "rg" {
   name     = random_pet.rg_name.id
   # Add Tags
   tags = {
-    "environment" = "k8sdev-5"
+    "environment" = "k8sdev-6"
     "demotag"     = "refreshtest"
     "main-tag"    = "main-value"
     # "portal-tag"  = "this is my value"
